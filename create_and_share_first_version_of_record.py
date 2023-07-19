83f7009d9e7e35190e1e61686d92dc37a8ed8873
Francois Liot <francois.liot@epfl.ch>
2023-07-19 17:30:42 +0200

    Inserted check of token's validity

--- create_and_share_first_version_of_record.py
+++ create_and_share_first_version_of_record.py
@@ -5,6 +5,26 @@ import requests
 import errno
 
 
+def check_token_validity(url, token):
+    """
+    ???
+    """
+    request_headers = {
+        "Accept": "application/json",
+        "Content-type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    # Send POST request
+    # with "verify=True" in production so that HTTPS requests verify SSL certificates
+    response = requests.get(
+        f'{url}/api/records?size=1',
+        headers=request_headers,
+        verify=True)
+
+    # Raise an exception if there is an HTTP error
+    response.raise_for_status()
+
 def check_input_folder_exists(input_folder_path):
     """
     Raises an exception if the input folder does not exist
@@ -12,21 +32,12 @@ def check_input_folder_exists(input_folder_path):
     if not os.path.exists(input_folder_path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), input_folder_path)
 
-def check_input_file_exists(input_folder_path):
+def check_input_file_exists(initial_metadata_file_path):
     """
-    Raises an exception if the record metadata file is missing from the input folder
+    Raises an exception if the initial metadata file is missing from the input folder
     """
-    if not os.path.isfile(record_metadata_file_path):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), record_metadata_file_path)
-
-def get_data_files(input_folder_path, record_metadata_file):
-    """
-    Returns the list of the files in the input folder, except the metadata file
-    """
-    all_files = [item for item in os.listdir(input_folder_path) if os.path.isfile(os.path.join(input_folder_path, item))]
-    data_files = [item for item in all_files if item != record_metadata_file]
-    return data_files
-
+    if not os.path.isfile(initial_metadata_file_path):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), initial_metadata_file_path)
 
 def create_draft_record(url, token, record_metadata_file_path):
     """
@@ -53,14 +64,21 @@ def create_draft_record(url, token, record_metadata_file_path):
         headers=request_headers,
         verify=True)
 
-    # Raise an exception if the record could not be created
-    if response.status_code != 201:
-        raise ValueError(f"Failed to create record (code: {response.status_code})")
+    # Raise an exception if there is an HTTP error
+    response.raise_for_status()
 
     #  Get record's id
     record_url = response.json()['links']['record']
     record_id = record_url.split('/api/records/')[-1]
     return record_id
+
+def get_data_files(input_folder_path, record_metadata_file):
+    """
+    Returns the list of the files in the input folder, except for the metadata file
+    """
+    all_files = [item for item in os.listdir(input_folder_path) if os.path.isfile(os.path.join(input_folder_path, item))]
+    data_files = [item for item in all_files if item != record_metadata_file]
+    return data_files
 
 def start_file_uploads(url, token, record_id, filenames):
     """
@@ -87,9 +105,8 @@ def start_file_uploads(url, token, record_id, filenames):
         headers=request_headers,
         verify=True)
 
-    # Raise an exception if the record could not be updated
-    if response.status_code != 201:
-        raise ValueError(f"Failed to update the record (code: {response.status_code})")
+    # Raise an exception if there is an HTTP error
+    response.raise_for_status()
 
 def upload_file_content(url, token, record_id, basedir, input_folder, filename):
     """
@@ -110,9 +127,8 @@ def upload_file_content(url, token, record_id, basedir, input_folder, filename):
             verify=True
         )
 
-    # Raise an exception if the file's content could not be uploaded
-    if response.status_code != 200:
-        raise ValueError(f"Upload of {filename}'s content failed (code: {response.status_code})")
+    # Raise an exception if there is an HTTP error
+    response.raise_for_status()
 
 
 def complete_file_upload(url, token, record_id, filename):
@@ -131,9 +147,8 @@ def complete_file_upload(url, token, record_id, filename):
         headers=request_headers,
         verify=True)
 
-    # Raise an exception if the upload of the file's content could not be completed
-    if response.status_code != 200:
-        raise ValueError(f"Completing the upload of {filename}'s content failed (code: {response.status_code})")
+    # Raise an exception if there is an HTTP error
+    response.raise_for_status()
 
 def publish_draft_record(url, token, record_id):
     """
@@ -151,9 +166,8 @@ def publish_draft_record(url, token, record_id):
         headers=request_headers,
         verify=True)
 
-    # Raise an exception if the draft could not be shared
-    if response.status_code != 202:
-        raise ValueError(f"Failed to publish record (code: {response.status_code})")
+    # Raise an exception if there is an HTTP error
+    response.raise_for_status()
 
 
 if __name__ == '__main__':
@@ -163,19 +177,19 @@ if __name__ == '__main__':
     # - the demo archive (main = False)
     main = False
 
-    # Specify a personal access token for the selected archive
-    # If you need a valid token, navigate to 'Applications' > 'Personal access tokens'
+    # Specify a valid token for the selected archive
+    # If you need a new token, navigate to 'Applications' > 'Personal access tokens'
     token = '<replace_with_token>'
 
     # Specify the folder where your input files are located
     input_folder = 'input'
 
-    # Specify the file in the input folder that contains the record's title, the record's authors...
+    # Specify the file in the input folder that contains the title, the authors... for the record
     # Note that all other files in the input folder will automatically be attached to the record
-    record_metadata_file = 'record_metadata.json'
+    initial_metadata_file = 'initial_metadata.json'
 
     # Specify whether you wish to share the record with all archive's users
-    publish = False
+    publish = True
 
     # ---------------Users: do not modify the information below---------------
     # Archives' urls
@@ -188,26 +202,28 @@ if __name__ == '__main__':
     logger.setLevel(logging.INFO)
 
     try:
+        if main:
+            url = main_archive_url
+
+        check_token_validity(url, token)
+
         basedir = os.path.abspath(os.path.dirname(__file__))
         input_folder_path = os.path.join(basedir, input_folder)
         check_input_folder_exists(input_folder_path)
         logger.info('Input folder exists')
 
-        record_metadata_file_path = os.path.join(basedir, input_folder, record_metadata_file)
-        check_input_file_exists(record_metadata_file_path)
-        logger.info(f'Input file {record_metadata_file} exists')
+        initial_metadata_file_path = os.path.join(basedir, input_folder, initial_metadata_file)
+        check_input_file_exists(initial_metadata_file_path)
+        logger.info(f'Input file {initial_metadata_file} exists')
 
-        if main:
-            url = main_archive_url
-
-        # Create a draft on the archive
-        # Get the record's id (e.g., 'cpbc8-ss975')
-        record_id = create_draft_record(url, token, record_metadata_file_path)
+        # Create a draft on the archive from the content of initial_metadata_file
+        # Get the id of the newly created record (e.g., 'cpbc8-ss975')
+        record_id = create_draft_record(url, token, initial_metadata_file_path)
         logger.info(f'Draft record with id={record_id} created with success')
 
-        # Update the record's metadata with the names of the data files to be attached to the record
-        # The data files are the files in the input folder, except for the record metadata file
-        data_files = get_data_files(input_folder_path, record_metadata_file)
+        # Update the record's metadata with the names of the data files that will be attached to the record
+        # The data files are the files in the input folder, except for the initial metadata file
+        data_files = get_data_files(input_folder_path, initial_metadata_file)
         data_files_paths = [os.path.join(input_folder_path, f) for f in data_files]
         start_file_uploads(url, token, record_id, data_files)
         logger.info('Data files specified with success')
@@ -224,7 +240,17 @@ if __name__ == '__main__':
             publish_draft_record(url, token, record_id)
             logger.info('Record published with success')
 
-    except FileNotFoundError as exception:
-        logger.error('File not found: ' + str(exception.filename))
-    except Exception as exception:
-        logger.error('Error occurred: ' + str(exception))
+    except requests.exceptions.HTTPError as e:
+        logger.error('Error occurred: ' + str(e))
+
+        status_code = e.response.status_code
+        reason = e.response.reason
+
+        if status_code == 403 and reason == 'FORBIDDEN':
+            logger.error('Check token\'s validity')
+
+    except FileNotFoundError as e:
+        logger.error('File not found: ' + str(e.filename))
+
+    except Exception as e:
+        logger.error('Error occurred: ' + str(e))
