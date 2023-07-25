a39cb4ca68796e1a030119326a7b78c133781e33
Francois Liot <francois.liot@epfl.ch>
2023-07-25 15:54:56 +0200

    Refactored retrieve_record.py

--- retrieve_record.py
+++ retrieve_record.py
@@ -25,8 +25,8 @@ def get_metadata_of_record(url, token, record_id):
 
     response.raise_for_status()
 
-    records_metadata = json.loads(response.text)
-    return records_metadata
+    metadata = json.loads(response.text)
+    return metadata
 
 def get_metadata_of_all_records(url, token, response_size):
     """
@@ -46,8 +46,8 @@ def get_metadata_of_all_records(url, token, response_size):
 
     response.raise_for_status()
 
-    records_metadata = json.loads(response.text)
-    return records_metadata
+    metadata = json.loads(response.text)
+    return metadata
 
 
 def get_metadata_of_all_latest_versions(url, token, response_size):
@@ -68,8 +68,8 @@ def get_metadata_of_all_latest_versions(url, token, response_size):
 
     response.raise_for_status()
 
-    records_metadata = json.loads(response.text)
-    return records_metadata
+    metadata = json.loads(response.text)
+    return metadata
 
 
 def recreate_folder(folder_path):
@@ -99,50 +99,46 @@ if __name__ == '__main__':
     response_size = int(1e6)
 
     try:
-        basedir = os.path.abspath(os.path.dirname(__file__))
-
-        # Read configuration file config.ini
+        # Read configuration file named config.ini
         config = configparser.ConfigParser()
+        basedir = os.path.abspath(os.path.dirname(__file__))
         config.read(os.path.join(basedir, 'config.ini'))
 
         # Create environment variables from secrets.env
         load_dotenv(os.path.join(basedir, 'secrets.env'))
-        logger.info('Environment variables created with success')
 
-        select_main_archive = config.get('general', 'select_main_archive')
+        # Get selected archive
+        selected_archive = config.get('general', 'selected_archive')
 
-        if select_main_archive == 'True':
+        if selected_archive == 'main':
             url = config.get('general', 'main_archive_url')
             token = os.getenv('MAIN_ARCHIVE_TOKEN')
-        elif select_main_archive == 'False':
+        elif selected_archive == 'demo':
             url = config.get('general', 'demo_archive_url')
             token = os.getenv('DEMO_ARCHIVE_TOKEN')
         else:
-            raise Exception('Invalid value for select_main_archive in config.ini')
+            raise Exception('Invalid selected_archive in config.ini')
 
-        # Get the metadata of published record(s)
-        requested_records = config.get('get_record_metadata', 'requested_records')
+        # Get metadata of requested records
+        requested_records = config.get('retrieve_record', 'requested_records')
 
         if requested_records == 'single':
-            record_id = config.get('get_record_metadata', 'record_id')
-            metadata = get_metadata_of_record(url, token, record_id)
+            id = config.get('retrieve_record', 'record_id')
+            metadata = get_metadata_of_record(url, token, id)
         elif requested_records == 'all':
             metadata = get_metadata_of_all_records(url, token, response_size)
         elif requested_records == 'latest':
             metadata = get_metadata_of_all_latest_versions(url, token, response_size)
         else:
-            raise Exception(f'Invalid value for requested_records: {requested_records}')
+            raise Exception(f'Invalid requested_records in config.ini')
 
-        logger.info('Metadata of shared record(s) obtained with success')
-
-        # Export the obtained metadata to a file in the output folder
-        output_folder = config.get('get_record_metadata', 'output_folder')
-        output_folder_path = os.path.join(basedir, output_folder)
-        recreate_folder(output_folder_path)
-        output_file = config.get('get_record_metadata', 'output_file')
+        # Export obtained metadata to file
+        output_folder = config.get('retrieve_record', 'output_folder')
+        output_file = config.get('retrieve_record', 'output_file')
         output_file_path = os.path.join(basedir, output_folder, output_file)
         export_to_file(output_file_path, metadata)
-        logger.info(f'Metadata exported to {output_file} with success')
+
+        logger.info(f'Metadata obtained and exported to {output_file}')
 
     except requests.exceptions.HTTPError as e:
         logger.error('Error occurred: ' + str(e))
