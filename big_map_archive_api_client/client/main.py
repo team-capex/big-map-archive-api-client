6bc137e26ddb2a95db12f2c6d906040cd2c621ad
Francois Liot <francois.liot@epfl.ch>
2023-08-04 19:20:44 +0200

    Refactored code using oo approach - WIP

--- big_map_archive_api_client/client/main.py
+++ big_map_archive_api_client/client/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import os
 from big_map_archive_api_client.client.client_config import ClientConfig
-from big_map_archive_api_client.utils import get_data_filenames, export_to_file
+from big_map_archive_api_client.utils import (get_data_filenames,
+                                              export_to_file)
 
 def create_record():
     """
@@ -10,13 +11,12 @@ def create_record():
     base_dir = Path(__file__).absolute().parent.parent.parent
     config_file_path = os.path.join(base_dir, 'config.yaml')
     client_config = ClientConfig.load_from_config_file(config_file_path)
-
     client = client_config.create_client()
 
     # Create draft from input metadata.json
     input_dir = client_config.input_dir
     metadata_filename = client_config.metadata_filename
-    response = client.post_record(base_dir, input_dir, metadata_filename)
+    response = client.post_records(base_dir, input_dir, metadata_filename)
     record_id = response['id']
     print(record_id)
 
@@ -42,8 +42,8 @@ def retrieve_record():
     base_dir = Path(__file__).absolute().parent.parent.parent
     config_file_path = os.path.join(base_dir, 'config.yaml')
     client_config = ClientConfig.load_from_config_file(config_file_path)
-
     client = client_config.create_client()
+
     record_id = client_config.requested_record_id
     response = client.get_record(record_id)
 
@@ -61,10 +61,10 @@ def retrieve_records():
     base_dir = Path(__file__).absolute().parent.parent.parent
     config_file_path = os.path.join(base_dir, 'config.yaml')
     client_config = ClientConfig.load_from_config_file(config_file_path)
+    client = client_config.create_client()
 
     all_versions = client_config.all_versions
     response_size = client_config.response_size
-    client = client_config.create_client()
     response = client.get_records(all_versions, response_size)
 
     export = client_config.export
@@ -77,12 +77,58 @@ def retrieve_records():
 
 def update_record():
     """
-
+    Updates a published record by either modifying the current version or creating a new version
     """
-    pass
+    base_dir = Path(__file__).absolute().parent.parent.parent
+    config_file_path = os.path.join(base_dir, 'config.yaml')
+    client_config = ClientConfig.load_from_config_file(config_file_path)
+    client = client_config.create_client()
+
+    published_record_id = client_config.published_record_id
+    same_version = client_config.same_version
+    input_dir = client_config.input_dir
+    metadata_filename = client_config.metadata_filename
+
+    if same_version:
+        # Create a draft (same version) and get the draft's id (same id)
+        response = client.post_draft(published_record_id)
+        record_id = response['id']
+
+        # Update the draft's metadata
+        client.update_metadata(record_id, base_dir, input_dir, metadata_filename)
+
+        # Publish the draft (update published record)
+        client.post_publish(record_id)
+    else:
+        # Create a draft (new version) and get its id
+        response = client.post_versions(published_record_id)
+        record_id = response['id']
+
+        # Update the draft's metadata
+        client.update_metadata(record_id, base_dir, input_dir, metadata_filename)
+
+        # Import all file links from the previous version
+        filenames = client.get_links(record_id)
+        client.delete_links(record_id, filenames)
+
+        client.post_file_import(record_id)
+
+        # Get list of links to remove and remove them
+        delete_missing = client_config.delete_missing
+        filenames = client.get_links_to_delete(record_id, delete_missing, base_dir, input_dir)
+        client.delete_links(record_id, filenames)
+
+        # Get list of files to upload and add links
+        filenames = client.get_files_to_link(record_id, delete_missing, base_dir, input_dir, metadata_filename)
+        client.add_links(record_id, filenames)
+
+
+
+
+
 
 if __name__ == '__main__':
     #create_record()
     #retrieve_record()
-    retrieve_records()
-    #update_record()
+    #retrieve_records()
+    update_record()
--- big_map_archive_api_client/utils/__init__.py
+++ big_map_archive_api_client/utils/__init__.py
@@ -1,7 +1,13 @@
-from .requests import generate_full_metadata, get_data_filenames, export_to_file
+from .requests import (generate_full_metadata,
+                       get_data_filenames,
+                       export_to_file,
+                       change_metadata,
+                       get_name_to_checksum_for_input_folder_files)
 
 __all__ = [
     'generate_full_metadata',
     'get_data_filenames',
-    'export_to_file'
+    'export_to_file',
+    'change_metadata',
+    'get_name_to_checksum_for_input_folder_files'
 ]
