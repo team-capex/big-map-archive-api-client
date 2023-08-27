21752b797956551da754d57a06079b1ac6161279
Francois Liot <francois.liot@epfl.ch>
2023-08-27 16:58:00 +0200

    Started to implement cmd_finales_db_copy()

--- cli/finales_db.py
+++ cli/finales_db.py
@@ -1,15 +1,15 @@
 import click
 from pathlib import Path
 import os
-import requests
+from datetime import datetime
 
 
 from cli.root import cmd_root
+from cli.record import cmd_record_create
 from big_map_archive_api_client.client.client_config import ClientConfig
-from big_map_archive_api_client.utils import (get_data_files_in_upload_dir,
-                                              export_to_json_file,
-                                              create_directory,
-                                              recreate_directory)
+from big_map_archive_api_client.utils import (export_to_json_file,
+                                              recreate_directory,
+                                              get_title_from_metadata_file)
 
 from finales_api_client.client.client_config import FinalesClientConfig
 
@@ -49,7 +49,9 @@ def cmd_finales_db():
     help='Relative path to the file that contains the metadata (title, list of authors, etc) for creating a new version.',
     type=click.Path(exists=True, file_okay=True, dir_okay=False),
 )
-def cmd_finales_db_copy(bma_config_file,
+@click.pass_context
+def cmd_finales_db_copy(ctx,
+                        bma_config_file,
                         finales_config_file,
                         record_id,
                         metadata_file):
@@ -88,3 +90,39 @@ def cmd_finales_db_copy(bma_config_file,
     results_filename = 'results_for_requests.json'
     results_file_path = os.path.join(base_dir_path, temp_dir_path, results_filename)
     export_to_json_file(base_dir_path, results_file_path, response)
+
+    # Create an ArchiveAPIClient object to interact with the archive
+    config_file_path = os.path.join(base_dir_path, bma_config_file)
+    client_config = ClientConfig.load_from_config_file(config_file_path)
+    client = client_config.create_client()
+
+    title = get_title_from_metadata_file(base_dir_path, metadata_file)
+
+    now = datetime.now()
+    additional_description = f' The back-up was performed on {now.strftime("%B %-d, %Y")} at {now.strftime("%H:%M")}.'
+
+    # If a record id is not provided
+    if record_id == '':
+        # Check whether the archive user already owns a published record with that title
+        record_ids = client.get_published_user_records_with_given_title(title)
+
+        # If there is no such a record, assume that the non-provided record id is intentional, otherwise ask for confirmation
+        if not record_ids:
+            # Create a new entry
+            ctx.invoke(cmd_record_create,
+                       config_file=bma_config_file,
+                       metadata_file=metadata_file,
+                       data_files=temp_dir_path,
+                       publish=True)
+        else:
+            # Ask for confirmation
+            click.echo(f'You already own a published record on the BIG-MAP Archive with the same title (record {record_ids[0]}).')
+            click.echo('But you did not provide that record id via the option "--record-id".')
+            click.confirm('Do you prefer creating a new entry rather than updating the existing one?', abort=True)
+
+            # Create a new entry
+            ctx.invoke(cmd_record_create,
+                       config_file=bma_config_file,
+                       metadata_file=metadata_file,
+                       data_files=temp_dir_path,
+                       publish=True)
